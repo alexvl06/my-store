0d1f4df3eafb1c04f5ce23ca658871437637a936
alexvl06 <alexisavila1991@gmail.com>
2023-06-03 10:46:52 -0500

    Setting configurations to follow vercel good practices.

--- api/index.js
+++ api/index.js
@@ -8,6 +8,10 @@ const port = process.env.PORT|3001;
 app.use(express.json())
 app.use(cors())
 
+app.get('/api', (req,res)=>{
+  res.send('Hi, this is my server in Express')
+})
+
 routerAPI(app)
 app.use(logErrors)
 app.use(boomErrorHandler)
